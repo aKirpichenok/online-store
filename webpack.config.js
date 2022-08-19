ae165372e2f01420dda9695e20e15c01bfa15704
aKirpichenok <a.kirpichenok99@gmail.com>
2022-08-19 13:49:36 +0300

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+
+
+
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development'
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+    entry: ["@babel/polyfill", path.resolve(__dirname, 'src', 'index.tsx')],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: "index.[contenthash].js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        }),
+        new MiniCssExtractPlugin({
+            filename: "index.[contenthash].css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader"
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [require("postcss-preset-env")]
+                            }
+                        }
+                    },
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.tsx?$/i,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    }
+}
