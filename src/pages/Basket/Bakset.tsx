42b24280a56c845a2ecef5833a4dec7d1389b377
aKirpichenok <a.kirpichenok99@gmail.com>
2022-08-28 10:28:05 +0300

    prod webpack

---
+++ src/pages/Basket/Bakset.tsx
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+import basket from '../../img/basket.svg';
+
+export const Basket = () => {
+    
+    const [open,setOpen] = useState(false)
+
+    return (
+        <div className="basket">
+            BASKET
+
+        </div>
+    )
+}
